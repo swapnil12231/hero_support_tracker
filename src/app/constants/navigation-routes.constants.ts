cc1f8c062104f2ac307bf30d8d9dc787a810048b
akhilesh.brahmankar@sumasoft.net <akhilesh.brahmankar@sumasoft.net>
2023-02-28 15:47:50 +0530

    Initial folder strucuture

---
+++ src/app/constants/navigation-routes.constants.ts
@@ -0,0 +1,1 @@
+export const navigationRoutes = Object.freeze({})
---
+++ src/app/modules/campaign/campaign.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CampaignRoutingModule } from './campaign-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    CampaignRoutingModule
+  ]
+})
+export class CampaignModule { }
