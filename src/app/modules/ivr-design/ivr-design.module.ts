34db39a34674eb82306fb9b5dc0df3c5dfcf5424
akhilesh.brahmankar@sumasoft.net <akhilesh.brahmankar@sumasoft.net>
2023-03-15 15:22:43 +0530

    Added remaining sidenav modules and their routing

---
+++ src/app/modules/ivr-design/ivr-design.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { IvrDesignRoutingModule } from './ivr-design-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    IvrDesignRoutingModule
+  ]
+})
+export class IvrDesignModule { }
---
+++ src/app/modules/real-time/real-time.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { RealTimeRoutingModule } from './real-time-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RealTimeRoutingModule
+  ]
+})
+export class RealTimeModule { }
